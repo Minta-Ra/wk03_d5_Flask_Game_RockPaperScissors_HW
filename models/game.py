c9448196d5f35d840151d576a8f15a08ed9d96b4
Raminta Razauskaite <r.razauskaite@gmail.com>
2021-11-27 15:47:05 +0000

    Add css - Finished extensions

--- models/game.py
+++ models/game.py
@@ -19,16 +19,16 @@ class Game():
             return (f"It is a draw! Both players chose to play {player_1.choice}")
 
         elif player_1.choice == roc and player_2.choice == pap:
-            return (f"{player_2.name} wins the game by choosing playing {player_2.choice}")
+            return (f"{player_2.name} wins the game by playing {player_2.choice}")
         elif player_1.choice == pap and player_2.choice == roc:
-            return (f"{player_1.name} wins the game by choosing playing {player_1.choice}")
+            return (f"{player_1.name} wins the game by playing {player_1.choice}")
 
         elif player_1.choice == pap and player_2.choice == sci:
-            return (f"{player_2.name} wins the game by choosing playing {player_2.choice}")
+            return (f"{player_2.name} wins the game by playing {player_2.choice}")
         elif player_1.choice == sci and player_2.choice == pap:
-            return (f"{player_1.name} wins the game by choosing playing {player_1.choice}")
+            return (f"{player_1.name} wins the game by playing {player_1.choice}")
 
         elif player_1.choice == sci and player_2.choice == roc:
-            return (f"{player_2.name} wins the game by choosing playing {player_2.choice}")
+            return (f"{player_2.name} wins the game by playing {player_2.choice}")
         elif player_1.choice == roc and player_2.choice == sci:
-            return (f"{player_1.name} wins the game by choosing playing {player_1.choice}")
+            return (f"{player_1.name} wins the game by playing {player_1.choice}")
